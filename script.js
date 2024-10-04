b2cdd3d9720fcc5763efb7c47a47d4111b51f72f
Mboni1 <mbonimpaclaude27@gmail.com>
2024-10-04 23:02:04 +0200

    strict and function

--- script.js
+++ script.js
@@ -459,3 +459,38 @@ const tip = bill <= 300 && bill >= 50 ? bill * 0.15 :
 console.log(`the bill was ${bill}, the tip was ${tip}
     ,and the total value ${bill + tip}`);
 //the bill was 275, the tip was 41.25, and the total value 316.25
+
+
+
+
+// Activating strict mode
+'Use strict';
+
+let hasDrivesLicence = false;
+const passTest = true;
+
+if (passTest) hasDrivesLicence = true;
+if (hasDrivesLicence) cosole.log('i can drive :D');// i can drive :D
+
+// functions
+
+function logger() {
+    console.log('My name is Jonas');
+}
+// calling / running / invoking function
+
+logger(); // My name is Jonas
+logger(); // My name is Jonas
+logger(); // My name is Jonas
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and
+     ${oranges} oranges.`;
+    return juice;
+}
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);// Juice with 5 apples and 0 oranges.
+
+const appleJuice = fruitProcessor(2, 4);
+console.log(appleJuice);// Juice with 2 apples and 4git oranges.
